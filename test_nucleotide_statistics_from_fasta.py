3306629f7ebf5ec64c31bcf2fab4a8b2c5d00a43
cyumuk97 <yumuk.c@northeastern.edu>
2021-03-07 11:48:22 -0500

    Updated Assignment 3

--- test_nucleotide_statistics_from_fasta.py
+++ test_nucleotide_statistics_from_fasta.py
@@ -6,6 +6,7 @@ from nucleotide_statistics_from_fasta import _get_nt_occurrence, _get_accession,
 
 sequence = "AGCTGCNCTAAATTGCGG"
 
+
 def test__get_nt_occurrence():
     """
     Tests if _get_nt_occurrence works correctly
@@ -13,14 +14,9 @@ def test__get_nt_occurrence():
     assert _get_nt_occurrence('A', sequence) == 2, "Problem on test__get_nt_occurrence"
     assert _get_nt_occurrence('A', sequence) == 4
 
+
 def test__get_accession():
     """
     Tests if _get_accession works correctly
     """
-
-
-def test_print_sequence_stats():
-    """
-    Tests if print_sequence_stats works correctly
-    """
-
+    assert _get_accession(sequence) == "GCTGCNCTA"
